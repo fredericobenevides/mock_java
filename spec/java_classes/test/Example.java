df566bbf01cf0c705b0c08ae565a6048b851aa55
Frederico Benevides <fredbene@gmail.com>
2015-05-18 16:39:20 -0300

    Just an example to see if java works

---
+++ spec/java_classes/test/Example.java
@@ -0,0 +1,11 @@
+package test;
+
+class Example {
+  public Example() {
+
+  }
+
+  public String hello_world() {
+    return "Hello World";
+  }
+}
--- spec/mock_java_spec.rb
+++ spec/mock_java_spec.rb
@@ -1,11 +1,20 @@
 require 'spec_helper'
 
+require 'mock_java'
+
+java_import 'test.Example'
+
 describe MockJava do
   it 'has a version number' do
     expect(MockJava::VERSION).not_to be nil
   end
 
   it 'does something useful' do
-    expect(false).to eq(true)
+    expect(true).to eq(true)
+  end
+
+  it 'should return hello world' do
+    msg = Example.new.hello_world
+    expect(msg).to eq('Hello World')
   end
 end
