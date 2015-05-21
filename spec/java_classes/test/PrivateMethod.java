efad41a7c316607f8df8584d48bc46098a39c28b
Frederico Benevides <fredbene@gmail.com>
2015-05-21 11:11:58 -0300

    Changed spaces to tab for java classes...

--- spec/java_classes/test/PrivateMethod.java
+++ spec/java_classes/test/PrivateMethod.java
@@ -2,24 +2,24 @@ package test;
 
 public class PrivateMethod {
 
-    private String className = "PrivateMethod";
+	private String className = "PrivateMethod";
 
-    public PrivateMethod() {
-    }
+	public PrivateMethod() {
+	}
 
-    private String getPrivateMethod() {
-        return this.className;
-    }
+	private String getPrivateMethod() {
+		return this.className;
+	}
 
-    private String getPrivateMethodArg1(String arg1) {
-        return arg1;
-    }
+	private String getPrivateMethodArg1(String arg1) {
+		return arg1;
+	}
 
-    private String getPrivateMethodArg2(String arg1, String arg2) {
-        return arg1 + " " + arg2;
-    }
+	private String getPrivateMethodArg2(String arg1, String arg2) {
+		return arg1 + " " + arg2;
+	}
 
-    private String[] getPrivateMethodVarargs(String... args) {
-        return args;
-    }
+	private String[] getPrivateMethodVarargs(String... args) {
+		return args;
+	}
 }
--- spec/java_classes/test/PublicMethod.java
+++ spec/java_classes/test/PublicMethod.java
@@ -2,24 +2,24 @@ package test;
 
 public class PublicMethod {
 
-    private String className = "PublicMethod";
+	private String className = "PublicMethod";
 
-    public PublicMethod() {
-    }
+	public PublicMethod() {
+	}
 
-    public String getPublicMethod() {
-        return this.className;
-    }
+	public String getPublicMethod() {
+		return this.className;
+	}
 
-    public String getPublicMethodArg1(String arg1) {
-        return arg1;
-    }
+	public String getPublicMethodArg1(String arg1) {
+		return arg1;
+	}
 
-    public String getPublicMethodArg2(String arg1, String arg2) {
-        return arg1 + " " + arg2;
-    }
+	public String getPublicMethodArg2(String arg1, String arg2) {
+		return arg1 + " " + arg2;
+	}
 
-    public String[] getPublicMethodVarargs(String... args) {
-        return args;
-    }
+	public String[] getPublicMethodVarargs(String... args) {
+		return args;
+	}
 }
